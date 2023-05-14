f0cd8de827449ead3370fa9a839a8c0a85e8c3d1
Nathan Donato <59492192+njd4dude@users.noreply.github.com>
2023-05-13 18:28:40 -0700

    privated env

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+require("dotenv").config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.on("ready", (c) => {
+  console.log(`${c.user.tag} is online`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+  
+  if (message.content === "ping") {
+    message.reply("ping");
+  }
+});
+
+client.login(process.env.TOKEN);
